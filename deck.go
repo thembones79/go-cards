78de3c13d1b3a14341c3f19fe22360ef03cb2dbb
thembones79 <michal.szumnarski@gmail.com>
2023-09-23 23:22:25 +0200

    newDeckFromFile

--- deck.go
+++ deck.go
@@ -39,3 +39,14 @@ func (d deck) toString() string {
 func (d deck) saveToFile(filename string) error {
 	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }
+
+func newDeckFromFile(filename string) deck {
+
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	return deck(strings.Split(string(bs), ","))
+
+}
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
+	// cards.print()
+ //    hand, remainingCards := deal(cards,5)
+ //    hand.print()
+ //    remainingCards.print()
+ //    cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
 	cards.print()
-    hand, remainingCards := deal(cards,5)
-    hand.print()
-    remainingCards.print()
-    cards.saveToFile("my_cards")
 }
 

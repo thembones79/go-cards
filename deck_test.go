43373b3e44bcc3dd0d131ba7d30c332edc213818
thembones79 <michal.szumnarski@gmail.com>
2023-10-03 17:12:48 +0200

    ended project

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -17,4 +20,19 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
 	}
 
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 16, but got {%v}", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
